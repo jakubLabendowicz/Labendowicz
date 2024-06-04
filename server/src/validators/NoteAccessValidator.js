48642f3181da0de579d2f6181ecc7507e6b91544
jakubLabedowicz <jalabedowicz@gmail.com>
2024-06-04 20:21:04 +0200

    remove

--- server/src/validators/NoteAccessValidator.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const createNoteAccessSchema = Joi.object({
-    userId: Joi.string().required().label("User Id"),
-})
-
-module.exports = {
-    createNoteAccessSchema
-}
